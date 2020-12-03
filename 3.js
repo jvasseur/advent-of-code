83c7644b5be8abe7e2de2a87917cff229eb14713
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2020-12-03 09:18:05 +0100

    Day 3

---
+++ 3.js
@@ -0,0 +1,23 @@
+const input = await Deno.readTextFile('3.txt');
+
+const grid = input.split('\n').filter((line) => line !== '').map((line) => line.split(''));
+
+const sloppe = (right, down) => {
+    let row = 0;
+    let col = 0;
+    let trees = 0;
+
+    while (row < grid.length) {
+        if (grid[row][col % grid[0].length] === '#') {
+            trees++;
+        }
+
+        row += down;
+        col += right;
+    }
+
+    return trees;
+}
+
+console.log(sloppe(3, 1));
+console.log(sloppe(1, 1) * sloppe(3, 1) * sloppe(5, 1) * sloppe(7, 1) * sloppe(1, 2));
