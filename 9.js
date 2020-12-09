99385763996aa2787e7d35dd9d9a6a7398c89e0b
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2020-12-09 12:05:22 +0100

    Day 9

---
+++ 9.js
@@ -0,0 +1,58 @@
+const input = (await Deno.readTextFile('9.txt')).split('\n').filter((line) => line !== '');
+
+const numbers = input.map((number) => parseInt(number, 10));
+
+const isValid = (number, queue) => {
+    for (const a of queue) {
+        for (const b of queue) {
+            if (number === a + b) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
+
+const queue = [];
+let invalid;
+
+for (const number of numbers) {
+    if (queue.length < 25) {
+        queue.push(number);
+
+        continue;
+    }
+
+    if (!isValid(number, queue)) {
+        invalid = number;
+
+        break;
+    }
+
+    queue.shift(number);
+    queue.push(number);
+}
+
+console.log(invalid);
+
+let vulnerability;
+
+for (const [key, number] of numbers.entries()) {
+    let sum = 0;
+    const suite = [];
+
+    for (let index = key; index < numbers.length && sum < invalid; index++) {
+        suite.push(numbers[index]);
+
+        sum += numbers[index];
+    }
+
+    if (sum === invalid) {
+        vulnerability = suite;
+
+        break;
+    }
+}
+
+console.log(Math.min(...vulnerability) + Math.max(...vulnerability));
