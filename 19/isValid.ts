d375df9b29ce96967b3de36eec3e050891c6e9c5
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2020-12-21 11:25:22 +0100

    Day 19 part 2

--- 19/isValid.ts
+++ 19/isValid.ts
@@ -1,15 +1,17 @@
 import { Rule } from './types.ts';
 
-const subValidation = (rules: Rule[], id: number, message: string, start: number): false|number => {
+const subValidation = (rules: Rule[], id: number, message: string, start: number[]): number[] => {
     const rule = rules[id];
 
     if (typeof rule === 'string') {
-        if (message[start] === rule) {
-            return start + 1;
-        } else {
-            return false;
-        }
+        const indexes = start
+            .filter((index) => message[index] === rule)
+            .map((index) => index + 1);
+
+        return indexes;
     }
+
+    let indexes: number[] = [];
 
     for (const option of rule) {
         let index = start;
@@ -18,7 +20,7 @@ const subValidation = (rules: Rule[], id: number, message: string, start: number
         for (const part of option) {
             const result = subValidation(rules, part, message, index);
 
-            if (result === false) {
+            if (result.length === 0) {
                 valid = false;
 
                 break;
@@ -28,13 +30,16 @@ const subValidation = (rules: Rule[], id: number, message: string, start: number
         }
 
         if (valid) {
-            return index;
+            indexes = [
+                ...indexes,
+                ...index,
+            ];
         }
     }
 
-    return false;
+    return indexes;
 };
 
-const isValid = (rules: Rule[], id: number, message: string): boolean => subValidation(rules, id, message, 0) === message.length;
+const isValid = (rules: Rule[], id: number, message: string): boolean => subValidation(rules, id, message, [0]).some((index) => index === message.length);
 
 export default isValid;
