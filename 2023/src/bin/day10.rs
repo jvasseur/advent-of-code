1417ae7da878367e5f1ada05a9462ffc0930668c
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2023-12-11 11:43:02 +0100

    Add 2023 day 10 part 1

---
+++ 2023/src/bin/day10.rs
@@ -0,0 +1,273 @@
+use advent_of_code_2023::{read, Parsable};
+use itertools::Itertools;
+use nom::bytes::complete::tag;
+use nom::IResult;
+use nom::combinator::value;
+use nom::multi::many0;
+use nom::sequence::terminated;
+use nom::branch::alt;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+struct Input {
+    lines: Vec<Vec<Item>>,
+}
+
+impl Parsable for Input {
+    fn parser(input: &str) -> IResult<&str, Self> {
+        let (input, lines) = many0(terminated(many0(Item::parser), tag("\n")))(input)?;
+
+        Ok((input, Input { lines }))
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+enum Item {
+    Pipe(Pipe),
+    Ground,
+    Start,
+}
+
+impl Parsable for Item {
+    fn parser(input: &str) -> IResult<&str, Self> {
+        alt((
+            value(Item::Pipe(Pipe::NS), tag("|")),
+            value(Item::Pipe(Pipe::EW), tag("-")),
+            value(Item::Pipe(Pipe::NE), tag("L")),
+            value(Item::Pipe(Pipe::NW), tag("J")),
+            value(Item::Pipe(Pipe::SW), tag("7")),
+            value(Item::Pipe(Pipe::SE), tag("F")),
+            value(Item::Ground, tag(".")),
+            value(Item::Start, tag("S")),
+        ))(input)
+    }
+
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum Pipe {
+    NS,
+    EW,
+    NE,
+    NW,
+    SW,
+    SE,
+}
+
+impl Pipe {
+    fn connections(&self, point: &Point) -> [Point; 2] {
+        match self {
+            Pipe::NS => [Point { line: point.line - 1, column: point.column }, Point { line: point.line + 1, column: point.column }],
+            Pipe::EW => [Point { line: point.line, column: point.column + 1 }, Point { line: point.line, column: point.column - 1 }],
+            Pipe::NE => [Point { line: point.line - 1, column: point.column }, Point { line: point.line, column: point.column + 1 }],
+            Pipe::NW => [Point { line: point.line - 1, column: point.column }, Point { line: point.line, column: point.column - 1 }],
+            Pipe::SW => [Point { line: point.line + 1, column: point.column }, Point { line: point.line, column: point.column - 1 }],
+            Pipe::SE => [Point { line: point.line + 1, column: point.column }, Point { line: point.line, column: point.column + 1 }],
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+struct Map {
+    lines: Vec<Vec<Option<Pipe>>>,
+    start: Point,
+}
+
+impl Map {
+    fn get(&self, point: &Point) -> Option<Pipe> {
+        self.lines[point.line][point.column]
+    }
+
+    fn connections(&self, point: &Point) -> Option<[Point; 2]> {
+        self.get(point).map(|pipe| pipe.connections(point))
+    }
+}
+
+impl From<&Input> for Map {
+    fn from(value: &Input) -> Self {
+        let mut start = None;
+
+        for line in 0..value.lines.len() {
+            for column in 0..value.lines[line].len() {
+                if value.lines[line][column] == Item::Start {
+                    start = Some(Point { line, column });
+                }
+            }
+        }
+
+        let start = start.unwrap();
+
+        let north = match value.lines[start.line - 1][start.column] {
+            Item::Pipe(pipe) => match pipe {
+                Pipe::NS => true,
+                Pipe::EW => false,
+                Pipe::NE => false,
+                Pipe::NW => false,
+                Pipe::SW => true,
+                Pipe::SE => true,
+
+            }
+            Item::Ground => false,
+            Item::Start => panic!("Multiple start"),
+        };
+
+        let south = match value.lines[start.line + 1][start.column] {
+            Item::Pipe(pipe) => match pipe {
+                Pipe::NS => true,
+                Pipe::EW => false,
+                Pipe::NE => true,
+                Pipe::NW => true,
+                Pipe::SW => false,
+                Pipe::SE => false,
+
+            }
+            Item::Ground => false,
+            Item::Start => panic!("Multiple start"),
+        };
+
+        let west = match value.lines[start.line][start.column - 1] {
+            Item::Pipe(pipe) => match pipe {
+                Pipe::NS => false,
+                Pipe::EW => true,
+                Pipe::NE => true,
+                Pipe::NW => false,
+                Pipe::SW => false,
+                Pipe::SE => true,
+
+            }
+            Item::Ground => false,
+            Item::Start => panic!("Multiple start"),
+        };
+
+        let east = match value.lines[start.line][start.column + 1] {
+            Item::Pipe(pipe) => match pipe {
+                Pipe::NS => false,
+                Pipe::EW => true,
+                Pipe::NE => false,
+                Pipe::NW => true,
+                Pipe::SW => true,
+                Pipe::SE => false,
+
+            }
+            Item::Ground => false,
+            Item::Start => panic!("Multiple start"),
+        };
+
+        let start_pipe = match (north, south, west, east) {
+            (true, true, false, false) => Pipe::NS,
+            (false, false, true, true) => Pipe::EW,
+            (true, false, false, true) => Pipe::NE,
+            (true, false, true, false) => Pipe::NW,
+            (false, true, true, false) => Pipe::SW,
+            (false, true, false, true) => Pipe::SE,
+            _ => panic!("Invalid start")
+        };
+
+        let lines = value.lines.iter().map(|line| line.iter().map(|item| match item {
+            Item::Pipe(pipe) => Some(*pipe),
+            Item::Ground => None,
+            Item::Start => Some(start_pipe),
+        }).collect()).collect();
+
+        Map { lines, start }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct Point {
+    line: usize,
+    column: usize,
+}
+
+fn solve_part1(input: &Input) -> u32 {
+    let map = Map::from(input);
+
+    let connections = map.connections(&map.start).unwrap();
+
+    let mut previous = map.start;
+    let mut current = connections[0];
+    let mut steps = 1;
+
+    while current != map.start {
+        let connections = map.connections(&current).unwrap();
+
+        let next = connections.into_iter().filter(|point| point != &previous).exactly_one().unwrap();
+
+        (previous, current) = (current, next);
+        steps += 1;
+    }
+
+    steps / 2
+}
+
+fn solve_part2(input: &Input) -> u32 {
+    let map = Map::from(input);
+
+    let mut enclosure: Vec<Vec<bool>> = map.lines.iter().map(|line| line.iter().map(|_| false).collect()).collect();
+
+    enclosure[map.start.line][map.start.column] = true;
+
+    let connections = map.connections(&map.start).unwrap();
+
+    let mut previous = map.start;
+    let mut current = connections[0];
+
+    while current != map.start {
+        enclosure[current.line][current.column] = true;
+
+        let connections = map.connections(&current).unwrap();
+
+        let next = connections.into_iter().filter(|point| point != &previous).exactly_one().unwrap();
+
+        (previous, current) = (current, next);
+    }
+
+    0
+}
+
+fn main() {
+    let input = read(10);
+
+    let parsed = Input::parse(&input).unwrap();
+
+    println!("{}", solve_part1(&parsed));
+    println!("{}", solve_part2(&parsed));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = ".....
+.S-7.
+.|.|.
+.L-J.
+.....
+";
+
+    fn parsed_input() -> Input {
+        Input {
+            lines: vec![
+                vec![Item::Ground, Item::Ground, Item::Ground, Item::Ground, Item::Ground],
+                vec![Item::Ground, Item::Start, Item::Pipe(Pipe::EW), Item::Pipe(Pipe::SW), Item::Ground],
+                vec![Item::Ground, Item::Pipe(Pipe::NS), Item::Ground, Item::Pipe(Pipe::NS), Item::Ground],
+                vec![Item::Ground, Item::Pipe(Pipe::NE), Item::Pipe(Pipe::EW), Item::Pipe(Pipe::NW), Item::Ground],
+                vec![Item::Ground, Item::Ground, Item::Ground, Item::Ground, Item::Ground],
+            ],
+        }
+    }
+
+    #[test]
+    fn test_parser() {
+        assert_eq!(Input::parse(INPUT), Ok(parsed_input()));
+    }
+
+    #[test]
+    fn test_solve_part1() {
+        assert_eq!(solve_part1(&parsed_input()), 4);
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        assert_eq!(solve_part2(&parsed_input()), 0);
+    }
+}
